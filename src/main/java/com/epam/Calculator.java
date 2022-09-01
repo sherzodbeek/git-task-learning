5ae97864c18d1b850c23698c207d8a76b26dd6c7
Sherzod Omonov <sherzodbeek98@gmail.com>
2022-09-01 18:25:49 +0500

    calculator created

---
+++ src/main/java/com/epam/Calculator.java
@@ -0,0 +1,20 @@
+package com.epam;
+
+public class Calculator {
+
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public int multiplyNumbers(int a, int b) {
+        return a * b;
+    }
+
+    public int subtractNumbers(int a, int b) {
+        return a - b;
+    }
+
+    public int divideNumbers(int a, int b) {
+        return a / b;
+    }
+}
